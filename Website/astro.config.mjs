d704bd99ebd251f080ed530cca3acfae224671e0
Max Tran <76268730+SpamMusubi153@users.noreply.github.com>
2023-09-24 13:24:36 -0700

    Attempt to put the base into the site url.

--- Website/astro.config.mjs
+++ Website/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://SpamMusubi153.github.io',
-    base: '/SUAnimal',
+    site: 'https://SpamMusubi153.github.io/SUAnimal',
+    // base: '/SUAnimal',
 
 });
