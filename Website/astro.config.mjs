ab901117617d3b07f77e8c657c0359f296ca21ed
Max Tran <76268730+SpamMusubi153@users.noreply.github.com>
2023-09-24 13:41:31 -0700

    Update site configuration

--- Website/astro.config.mjs
+++ Website/astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://SpamMusubi153.github.io',
     base: '/SUAnimal',
 
-});
+})
