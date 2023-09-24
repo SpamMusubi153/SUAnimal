9cbef54d8a13201ccdc4ba56dbe99d3810ed6ab8
Max Tran <76268730+SpamMusubi153@users.noreply.github.com>
2023-09-24 13:52:34 -0700

    Update configuration for flexibility

--- Website/astro.config.mjs
+++ Website/astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://SpamMusubi153.github.io',
-    base: '/SUAnimal',
+    // The site and the base path should automatically be provided at build time with GitHub Actions.
+    // site: 'https://SpamMusubi153.github.io',
+    // base: '/SUAnimal',
 
-})
+});
