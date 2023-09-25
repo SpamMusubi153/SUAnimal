0fa2c58952a986bb368368d6ccfc9d7cf0356e9b
Max <76268730+SpamMusubi153@users.noreply.github.com>
2023-09-24 18:42:19 -0700

    Explicitly specify the site and the base path for development.

--- Website/astro.config.mjs
+++ Website/astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    // The site and the base path should automatically be provided at build time with GitHub Actions.
-    // site: 'https://SpamMusubi153.github.io',
-    // base: '/SUAnimal',
+    // The site and the base path should also automatically be provided at build time with GitHub Actions.
+    site: 'https://spammusubi153.github.io',
+    base: '/SUAnimal',
 
 });
